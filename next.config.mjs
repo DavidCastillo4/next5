4db4858191c9b1480413654cc28a819cbc2dfdd7
David Castillo <david.castillo4@gmail.com>
2025-06-13 16:50:45 -0500

    zz

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,5 @@
-const nextConfig = {};
+const nextConfig = {
+ // basePath: '/next5' ,
+   assetPrefix: '/next5'
+};
 export default nextConfig;
---
+++ server.js
@@ -0,0 +1,18 @@
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const port = 3117; // YOUR port
+const dev = false;  //process.env.NODE_ENV !== 'production';
+console.log(dev)
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
