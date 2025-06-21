47e7c74b0f330d3228711687dc70f7027ead95eb
David Castillo <david.castillo4@gmail.com>
2025-06-21 12:37:41 -0500

    66644

--- src/Comps/Nav/navigation.js
+++ src/Comps/Nav/navigation.js
@@ -11,14 +11,14 @@ export let Navigation = () => {
  return (
   <nav>
    <Link className={css.moduleGreen} href='/' >Home</Link>
-   <Link className='globalPink' href='/Contact' >Contact</Link>
-   <Link className='bg-amber-500' href='/Jotai' >Jotai</Link>
-   <Link className='bg-green-500' href='/Jotai2' >Jotai2</Link>
+   <Link className='globalPink' href='/contact2' >Contact</Link>
+   <Link className='bg-amber-500' href='/jotai333' >Jotai</Link>
+   <Link className='bg-green-500' href='/jotai222' >Jotai2</Link>
    <Link className='bg-slate-300' href='/client' >Client</Link>
-   <Link className={css.moduleYellow} href='/Dashboard' >Dashboard</Link>
-   <Link className={css.moduleRed} href='/Product' >Product</Link>
-   <Link className='globalBlue' href='/Login' >Login</Link>
-   <Link className='bg-gray-200' href='/Register' >Register</Link>
+   <Link className={css.moduleYellow} href='/dashboard44' >Dashboard</Link>
+   <Link className={css.moduleRed} href='/product2' >Product</Link>
+   <Link className='globalBlue' href='/login2' >Login</Link>
+   <Link className='bg-gray-200' href='/register2' >Register</Link>
   </nav>
  )
 };
