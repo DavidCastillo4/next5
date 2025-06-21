8ffc7865c28f42fb04d76488af0913778190c7ba
David Castillo <david.castillo4@gmail.com>
2025-06-21 12:53:12 -0500

    444

--- src/app/product55/[productId]/page.js
+++
@@ -1,6 +0,0 @@
-/*Product\[productId]\page.js*/
-export default async function ProductDetails({ params }) {
- const {productId} = await params;
- return <div>product {productId}</div>;
-}
-
--- src/app/product55/page.js
+++
@@ -1,4 +0,0 @@
-/*Product\page.js*/
-export default function Product() {
- return (<div>Product</div>);
-}
